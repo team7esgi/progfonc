d351323acf1faf8f8170d4fa3c8461f12986c0e9
Joseph CHATELIN <josephchatelin@gmail.com>
2021-01-21 05:02:00 +0100

    finish scala project

--- src/main/scala/projetal2020/FileReader.scala
+++ src/main/scala/projetal2020/FileReader.scala
@@ -22,51 +22,120 @@ object FileReader {
 
   def readInstructions(fileName: String): Try[JsObject] = {
     readFile(fileName) match {
-      case Success(n) => executeInstructions(n)
-      case Failure(e) => Failure(new Exception(e.getMessage))
+      case Success(instructions) => launchMower(instructions)
+      case Failure(exception)    => Failure(new Exception(exception.getMessage))
     }
   }
 
-  def executeInstructions(instructions: List[String]): Try[JsObject] = {
-
+  def launchMower(instructions: List[String]): Try[JsObject] = {
     if (instructions.size % 2 == 0) {
       Failure(
         new DonneesIncorrectesException(
-          "Le information du fichier ne sont pas correctes !"
+          "Les informations du fichier ne sont pas correctes."
         )
       )
     } else {
-      val outputMower: ArrayBuffer[JsObject] = new ArrayBuffer[JsObject]()
-      val lineSplit: Array[String] = instructions(0).split(" ")
-      val map: Grid = Grid(lineSplit(0).toInt, lineSplit(1).toInt)
-      val linesSliced = instructions.slice(1, instructions.size).grouped(2)
-
-      val limite = Json.obj("x" -> lineSplit(0), "y" -> lineSplit(1))
-
-      for (mowerInstructions <- linesSliced) {
-        //println(mowerInstructions(0) + " : " + mowerInstructions(1))
-        val mowerInfo = mowerInstructions(0).split(" ")
-        val mower = map.createMower(
-          mowerInfo(0).toInt,
-          mowerInfo(1).toInt,
-          mowerInfo(2).toCharArray()(0)
-        )
-        map.moveMower(mowerInstructions(1), mower)
-
-        val result = Json.obj(
-          "debut" ->
-            Json.obj(
-              "point" -> Json
-                .obj("x" -> mowerInfo(0).toInt, "y" -> mowerInfo(1).toInt),
-              "direction" -> mowerInfo(2).toCharArray()(0).toString
-            ),
-          "instructions" -> mowerInstructions(1),
-          "fin"          -> mower.info
-        )
-        outputMower.append(result)
+      val mowerInfoAndInstructions =
+        instructions.slice(1, instructions.size).grouped(2).toList
+      checkGridBoundaryInfo(instructions(0)) match {
+        case Success(info) =>
+          executeInstructions(info, mowerInfoAndInstructions)
+        case Failure(exception) =>
+          Failure(new Exception(exception.getMessage))
       }
-      val result = Json.obj("limite" -> limite, "tondeuses" -> outputMower)
-      Success(result)
     }
+  }
+
+  def checkGridBoundaryInfo(gridInfo: String): Try[(Int, Int)] = {
+    try {
+      val infos = gridInfo.trim.split(" ")
+      if (infos.size > 2) {
+        Failure(
+          new DonneesIncorrectesException(
+            "Les données des limites de la grille sont invalides."
+          )
+        )
+      } else {
+        Success((infos(0).toInt, infos(1).toInt))
+      }
+    } catch {
+      case _: Throwable =>
+        Failure(
+          new DonneesIncorrectesException(
+            "Les données des limites de la grille sont invalides."
+          )
+        )
+    }
+  }
+
+  def checkMowerInitialInfo(mowerInfo: String): Try[(Int, Int, Char)] = {
+    try {
+      val infos = mowerInfo.trim.split(" ")
+      if (infos.size > 3 || infos(2).toCharArray()(0).isDigit) {
+        Failure(
+          new DonneesIncorrectesException(
+            "Les données de la tondeuse sont invalides."
+          )
+        )
+      } else {
+        Success(
+          (
+            infos(0).toInt,
+            infos(1).toInt,
+            infos(2).toCharArray()(0)
+          )
+        )
+      }
+    } catch {
+      case _: Throwable =>
+        Failure(
+          new DonneesIncorrectesException(
+            "Les données de la tondeuse sont invalides"
+          )
+        )
+    }
+  }
+
+  def executeInstructions(
+      gridBound: (Int, Int),
+      mowerInfoAndInstructions: List[List[String]]
+  ): Try[JsObject] = {
+    val outputMower: ArrayBuffer[JsObject] = new ArrayBuffer[JsObject]()
+    val grid: Grid = Grid(gridBound._1, gridBound._2)
+    val limite = Json.obj("x" -> gridBound._1, "y" -> gridBound._2)
+
+    for (mowerInfos <- mowerInfoAndInstructions) {
+      checkMowerInitialInfo(mowerInfos(0)) match {
+        case Success(info) =>
+          outputMower.insert(
+            outputMower.size,
+            InitAndMoveMower(grid, info, mowerInfos(1))
+          )
+        case Failure(exception) => println(exception.getMessage)
+      }
+    }
+    val result = Json.obj("limite" -> limite, "tondeuses" -> outputMower)
+    Success(result)
+  }
+
+  def InitAndMoveMower(
+      grid: Grid,
+      mowerInfo: (Int, Int, Char),
+      instructions: String
+  ): JsObject = {
+    val mower = grid.createMower(mowerInfo._1, mowerInfo._2, mowerInfo._3)
+    grid.moveMower(instructions, mower)
+
+    val result = Json.obj(
+      "debut" ->
+        Json.obj(
+          "point" -> Json
+            .obj("x" -> mowerInfo._1, "y" -> mowerInfo._2),
+          "direction" -> mowerInfo._3.toString
+        ),
+      "instructions" -> instructions,
+      "fin"          -> mower.info
+    )
+    result
   }
 }
